4314dea7823aad15cf247a745a10219f91663b86
rohanawan <rohan.awan863@gmail.com>
2025-08-14 20:10:32 +0100

    fixes

--- src/components/home/cta.tsx
+++ src/components/home/cta.tsx
@@ -1,24 +1,22 @@
 import styles from "../../styles/banner.module.css";
 
 export default function Cta() {
-    return (
-        <div className={styles.cta}>
-            <a
-                href="https://pretix.eu/apostrophdesign/houseparty/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.button}
-            >
-                get your ticket
-            </a>
+  return (
+    <div className={styles.cta}>
+        <a
+          href="https://pretix.eu/apostrophdesign/houseparty/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.button}
+        >
+          get your ticket
+        </a>
 
-            <div className={styles.limitedMerchContainer}>
-                <div className={styles.dot} />
-                <p className={styles.limitedMerch}>
-                    Limited Merch + Tattoo Station!!
-                </p>
-                <div className={styles.dot} />
-            </div>
-        </div>
-    );
+      <div className={styles.limitedMerchContainer}>
+        <div className={styles.dot} />
+        <p className={styles.limitedMerch}>Limited Merch + Tattoo Station!!</p>
+        <div className={styles.dot} />
+      </div>
+    </div>
+  );
 }
